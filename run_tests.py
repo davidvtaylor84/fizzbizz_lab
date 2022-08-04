045944aa398f1a3c45792568d874b48db3a3851b
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-08-04 10:43:20 +0100

    first save

---
+++ run_tests.py
@@ -0,0 +1,8 @@
+
+
+import unittest
+
+from tests.fizzbizz_test import TestFizzbizz
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/fizzbizz_test.py
@@ -0,0 +1,10 @@
+import unittest
+from src.fizzbizz import Fizzbizz
+
+class TestFizzbizz(unittest.TestCase):
+
+    def setUp(self):
+        self.fizzbizz = (10)
+        self.fizzbizz2 =(9)
+
+        
