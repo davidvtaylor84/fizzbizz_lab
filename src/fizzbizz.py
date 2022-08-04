2f128a52fe42d5a3ec8fef757b740eaaae55d85f
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-08-04 13:05:22 +0100

    finished fizzbizz

--- src/fizzbizz.py
+++ src/fizzbizz.py
@@ -1,2 +1,11 @@
-class Fizzbizz:
-    pass
+
+    
+def fizzbizz(number):
+    if number %3 ==0 and number %5 == 0:
+        return "Fizzbizz"
+    elif number %3 == 0:
+        return "Fizz"
+    elif number %5 == 0:
+        return "Bizz"
+    else:
+        return str(number)
--- tests/fizzbizz_test.py
+++ tests/fizzbizz_test.py
@@ -1,10 +1,20 @@
 import unittest
-from src.fizzbizz import Fizzbizz
+from src.fizzbizz import *
 
 class TestFizzbizz(unittest.TestCase):
 
-    def setUp(self):
-        self.fizzbizz = (10)
-        self.fizzbizz2 =(9)
+    def test_fizz(self):
+        number = fizzbizz(3)
+        self.assertEqual("Fizz", number)
 
-        
+    def test_bizz(self):
+        number = fizzbizz(10)
+        self.assertEqual("Bizz", number)
+
+    def test_fizzbizz(self):
+        number = fizzbizz(15)
+        self.assertEqual("Fizzbizz", number)
+
+    def test_error(self):
+        number = fizzbizz(4)
+        self.assertEqual(str(number), number)
